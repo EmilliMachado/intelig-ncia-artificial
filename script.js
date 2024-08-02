24d76b437791e2faac264221b45a9d330e7cb4e4
emy <106605303+EmilliMachado@users.noreply.github.com>
2024-08-02 11:10:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,11 +10,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Sim, acredito que ela não é capaz.",
-                afirmacao: "está sendo preconceituoso(a), por julgar ela de não ser capaz de tal atividade"
+                afirmacao: [
+                    "está sendo preconceituoso(a), por julgar ela de não ser capaz de tal atividade.",
+                    "por julgar de forma errônea, está sendo preconceituoso(a), pois acha que ela não é capaz de tal atividade."
+                ]
             },
             {
                 texto: "Não, não acredito.",
-                afirmacao: "sem preconceitos, você entende que todos são capazes de praticar esportes"
+                afirmacao: [
+                    "sem preconceitos, você entende que todos são capazes de praticar esportes.",
+                    "com a mente aberta, você sabe que todos tem o direito de praticar esportes e sabe que apesar de algumas limitações ele pode sim jogar basquete."
+                ]
             }
         ]
     },
@@ -23,11 +29,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Sim, infelizmente já vi isso acontecer.",
-                afirmacao: "mesmo que houve varias mudanças, ainda há muito preconceito em nosso dia a dia"
+                afirmacao: [
+                    "mesmo que houve varias mudanças, ainda há muito preconceito em nosso dia a dia.",
+                    "mesmo com a evolução da sociedade, incluindo os que tem deficiência física em muitas atividades, ainda se vê muito preconceito em relação ao modo de ser tratados."
+                ]
             },
             {
                 texto: "Não, nunca presenciei algo assim.",
-                afirmacao: "muitas pessoas hoje em dia mudaram sua forma de pensar, incluindo todos sem nenhum tipo de preconceitos."
+                afirmacao: [
+                    "muitas pessoas hoje em dia mudaram sua forma de pensar, incluindo todos sem nenhum tipo de preconceitos.",
+                    "com a evolução da sociedade, podemos ver a inclusão dos deficientes físicos em muitas atividades, com tratamento igual para igual."
+                ]
             }
         ]
     },
@@ -36,11 +48,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Sim, são ofensivas.",
-                afirmacao: "piadinhas não podem ser aceita, pois todos somos iguais."
+                afirmacao: [
+                    "piadinhas não podem ser aceita, pois todos somos iguais.",
+                    "fazer piadinhas é totalmente errado, pois todos somos iguais, vivemos em um mundo com os mesmos direitos."
+                ]
             },
             {
                 texto: "Não vejo problema nelas.",
-                afirmacao: "por se acharem melhor, fazem piadinhas ridicularizando o proximo, isso é totalmente errado."
+                afirmacao: [
+                    "por se acharem melhor, fazem piadinhas ridicularizando o proximo, isso é totalmente errado.",
+                    "por se julgarem melhor que as pessoas com deficiência, fazem piadinhas os ridicularizando, sem pensar nas consequências que isso vai ter."
+                ]
             }
         ]
     },
@@ -49,11 +67,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Sim, acredito.",
-                afirmacao: "com pensamento errado, julgam as pessoas com deficiência como uma carga."
+                afirmacao: [
+                "com pensamento errado, julgam as pessoas com deficiência como uma carga para a sociedade.",
+                "A visão de pessoas com deficiência como uma 'carga' é baseada em estereótipos ultrapassados e não reconhece suas capacidades e potenciais contribuições."
+            ]
             },
             {
                 texto: "Não, não acredito.",
-                afirmacao: "pessoas com deficiência não são de forma alguma uma carga para a sociedade."
+                afirmacao: [
+                    "pessoas com deficiência não são de forma alguma uma carga para a sociedade.",
+                    "Pessoas com deficiência possuem habilidades únicas e contribuem de maneira valiosa para a diversidade e inclusão na sociedade."
+                ]
             }
         ]
     },
@@ -62,11 +86,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Sim, todos merecem as mesmas chances.",
-                afirmacao: "independente de suas capacidades físicas, devem ter várias oportunidades, pois os direitos são iguais para todos."
+                afirmacao: [
+                    "independente de suas capacidades físicas, devem ter várias oportunidades, pois os direitos são iguais para todos.",
+                    "Todos os indivíduos merecem ter acesso igualitário a oportunidades, independentemente de suas capacidades físicas, para promover uma sociedade justa e inclusiva."
+                ]
             },
             {
                 texto: "Não acho que isso seja possível.",
-                afirmacao: "de forma errônea diminuem a capacidade das pessoas com deficiência, dizem não ser possivel incluir e dar oportunidades."
+                afirmacao: [
+                    "de forma errônea diminuem a capacidade das pessoas com deficiência, dizendo não ser possivel incluir na sociedade e dar oportunidades.",
+                    "preconceituosamente, muitos dizem que pessoas com deficiência física não merecem oportunidades, pois são limitados."
+                ]
             }
         ]
     },
@@ -98,7 +128,7 @@ function mostraAlternativas(){
 }
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
@@ -109,5 +139,11 @@ function mostraResultado() {
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
+
+function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
 
 mostraPergunta();
